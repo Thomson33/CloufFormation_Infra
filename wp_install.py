3ef30968594df104a5b13454b40140601044b486
Antoine <ant.vente@gmail.com>
2019-01-30 11:09:24 +0100

    Update wp_install.py

--- wp_install.py
+++ wp_install.py
@@ -4,7 +4,7 @@ import requests
 """ Download and install WORDPRESS. """
 
 os.system("sudo wget https://wordpress.org/latest.tar.gz -P /var/www)
-os.system("sudo tar zxvf /var/www/html/{}/latest.tar.gz -C /var/www)
+os.system("sudo tar zxvf /var/www/latest.tar.gz -C /var/www)
 
 secret_key = 'https://api.wordpress.org/secret-key/1.1/salt/'
 r = requests.post(secret_key)
@@ -18,7 +18,7 @@ line = 'not_empty'
 
 while line:
 	line = wp_non_conf.readline()
-	wp_conf.write(line.format(db_name=WP_DB_NAME,db_user=WP_DB_USER,db_mdp=WP_DB_PASS,key_secret=r.text))
+	wp_conf.write(line.format(db_name='zz',db_user='ss',db_mdp='zz',key_secret=r.text))
 
 wp_conf.close()
 
